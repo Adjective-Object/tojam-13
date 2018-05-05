7ea28e6e83349bc8335384a2a496caa3882d33b1
artur-kink <artur.kink@gmail.com>
2018-05-04 20:36:03 -0400

    Following camera

---
+++ Assets/Scenes/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+
+    public MonoBehaviour player;
+    Vector3 delta;
+
+	// Use this for initialization
+	void Start () {
+        delta = transform.position - player.transform.position;
+        delta.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocity = Vector3.zero;
+        Vector3 target = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z) + delta;
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.1f);
+    }
+}
