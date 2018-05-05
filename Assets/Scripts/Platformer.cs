9108eea22b5725941d839f18d7d4aa351b2ae0ac
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2018-05-04 20:37:40 -0400

    we did it fam

--- Assets/Scripts/Platformer.cs
+++ Assets/Scripts/Platformer.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Platformer : MonoBehaviour {
 
+	public UVAnimation animator;
+
 	public float fallspeed = 1f;
 	public float walkspeed = 0.5f;
 
 	public float friction = 0.2f;
+
+	public float standThreshold = 0.0001f;
 
 	private Vector2 velocity = new Vector2(0, 0);
 
@@ -19,10 +24,17 @@ public class Platformer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 iv= GetIntendedVelocity();
-		Debug.Log(iv);
 		velocity += iv * Time.deltaTime;
 		velocity *= 1 - friction;
 		transform.position += new Vector3(velocity.x, velocity.y, 0);
+
+		float speed = velocity.magnitude;
+		if (Math.Abs(speed - 0f) < standThreshold) {
+			animator.Reset();
+			animator.SetAnimationSpeed(0);
+		} else {
+			animator.SetAnimationSpeed(speed);
+		}
 	}
 
 	Vector2 GetIntendedVelocity() {
